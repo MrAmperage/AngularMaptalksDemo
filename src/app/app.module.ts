e84c203b8647a6ccacd65e8fab0b11589f4e19d3
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2024-09-26 17:17:10 +0700

    Заготовка для теста карты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AngularMaptalksModule } from 'angular-maptalks';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AngularMaptalksModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
