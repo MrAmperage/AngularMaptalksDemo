9adc0df21862cf60d5580cde7ef57581c238eed8
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2024-09-29 15:24:32 +0700

    Исправлено отображение каты и временно убрал инициализацию карты с параметрами из сервиса

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MapService } from 'angular-maptalks';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private MapService: MapService) {}
-  ngOnInit(): void {
-    this.MapService.SetConfiguration({
-      Zoom: 19,
-      Center: [55.14, 86.07],
-      BaseTileLayerUrlTemplate:
-        'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-    });
-    console.log(this.MapService);
-  }
-}
+export class AppComponent {}
