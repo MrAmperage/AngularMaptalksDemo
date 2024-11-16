ce865ad734996ec13bfeff30207f357f0c68304a
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2024-11-16 17:10:36 +0700

    SmallFix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapService } from 'angular-maptalks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private MapService: MapService) {}
+
+  ngOnInit(): void {
+    this.MapService.SetConfiguration({ Center: [86.7, 55.14], Zoom: 19 });
+  }
+}
