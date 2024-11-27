8be167fa85ba737627ed59341b955d50cbd65e42
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2024-11-27 11:40:33 +0700

    Мелкие исправления в приложении

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AngularMaptalksModule, MapService } from 'angular-maptalks';
+import { AngularMaptalksToolbarPluginModule } from 'angular-maptalks-toolbar-plugin';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AngularMaptalksModule],
+  imports: [
+    BrowserModule,
+    AngularMaptalksModule,
+    AngularMaptalksToolbarPluginModule,
+  ],
   providers: [MapService],
   bootstrap: [AppComponent],
 })
